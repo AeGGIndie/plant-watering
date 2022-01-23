9d804ea5aec2357d46ff3541032dfe4c38767470
AeGGIndie <37030567+AeGGIndie@users.noreply.github.com>
2022-01-23 14:38:11 -0500

    added basic backend routes using minimal webapi c#

---
+++ Migrations/20220123184536_initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace plant_watering.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plants",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    lastWatered = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plants", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plants");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,120 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+/* Injections */
+var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+builder.Services.AddDbContext<ApiDbContext>(options =>
+  options.UseSqlite(connectionString));
+
+// builder.Services.AddSingleton<PlantRepository>();
+var app = builder.Build();
+
+app.MapGet("/plants", async (ApiDbContext db) =>
+{
+  return await db.Plants.ToListAsync();
+});
+
+app.MapGet("/plants/{id}", async (ApiDbContext db, int id) =>
+{
+  var plant = await db.Plants.FirstOrDefaultAsync(p => p.Id == id);
+  return plant == null ? Results.NotFound() : Results.Ok(plant);
+});
+
+app.MapPost("/plants", async (ApiDbContext db, int id) =>
+{
+  if (await db.Plants.FirstOrDefaultAsync(p => p.Id == id) != null)
+  {
+    return Results.BadRequest();
+  }
+
+  Plant newPlant = new Plant(id, DateTime.UtcNow);
+  db.Plants.Add(newPlant);
+  await db.SaveChangesAsync();
+
+  return Results.Created($"/plants/{newPlant.Id}", newPlant);
+});
+
+app.MapPut("/plants/{id}", async (ApiDbContext db, int id) =>
+{
+  var plant = await db.Plants.FirstOrDefaultAsync(p => p.Id == id);
+  if (plant == null)
+  {
+    return Results.NotFound();
+  }
+
+  plant.LastWatered = DateTime.UtcNow;
+  await db.SaveChangesAsync();
+
+  return Results.Ok(plant);
+});
+
+app.MapDelete("/plants/{id}", async (ApiDbContext db, int id) =>
+{
+  var plant = await db.Plants.FirstOrDefaultAsync(p => p.Id == id);
+  if (plant == null)
+  {
+    return Results.BadRequest();
+  }
+
+  db.Plants.Remove(plant);
+  await db.SaveChangesAsync();
+  return Results.NoContent();
+});
+app.Run();
+
+
+class Plant
+{
+  public int Id { get; set; }
+  public DateTime LastWatered { get; set; }
+
+  public Plant(int Id, DateTime LastWatered)
+  {
+    this.Id = Id;
+    this.LastWatered = LastWatered;
+  }
+}
+
+/* In-memory database */
+// class PlantRepository
+// {
+//   private Dictionary<int, Plant> plants = new Dictionary<int, Plant>();
+
+//   /* Repository */
+//   public PlantRepository()
+//   {
+//     var plant1 = new Plant(1, DateTime.UtcNow);
+//     var plant2 = new Plant(2, DateTime.UtcNow);
+//     var plant3 = new Plant(3, DateTime.UtcNow);
+
+//     plants.Add(plant1.id, plant1);
+//     plants.Add(plant2.id, plant2);
+//     plants.Add(plant3.id, plant3);
+//   }
+
+//   public IEnumerable<Plant> GetAll() => plants.Values;
+
+//   public Plant GetById(int id)
+//   {
+//     if (plants.ContainsKey(id))
+//     {
+//       return plants[id];
+//     }
+//     return null;
+//   }
+
+//   public void Add(Plant plant) => plants.Add(plant.id, plant);
+//   public void Update(Plant plant) => plants[plant.id] = plant;
+
+// }
+
+/* Database */
+class ApiDbContext : DbContext
+{
+  public DbSet<Plant> Plants { get; set; }
+
+  public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+}
